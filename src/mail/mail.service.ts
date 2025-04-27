578feb5c822301326e7b7b20a28935d84cc42092
NorikAleqsanyan <aleqsanyan.004@gmail.com>
2025-04-27 19:24:21 +0300

    Added comments to MailService methods using JSDoc format

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,3 @@
-// src/mail/mail.service.ts
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
 
@@ -6,6 +5,15 @@ import { Injectable } from '@nestjs/common';
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
+  /**
+   * Sends an email to the specified recipient.
+   *
+   * @param email - The recipient's email address.
+   * @param subject - The subject of the email.
+   * @param html - The HTML content of the email body.
+   * @returns A promise indicating the completion of the email sending process.
+   * @throws return message: or handle errors related to sending the email.
+   */
   async sendEmail(
     email: string,
     subject: string,

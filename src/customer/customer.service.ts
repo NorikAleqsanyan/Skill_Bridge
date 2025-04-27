8f88e11303083dbe6fe4bda02b9ddeefe5898f62
NorikAleqsanyan <aleqsanyan.004@gmail.com>
2025-04-27 19:22:42 +0300

    Added comments to CustomerService methods using JSDoc format

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -8,10 +8,24 @@ export class CustomerService {
   constructor(
     @InjectModel('Customer') private customerModel: Model<Customer>,
   ) {}
+
+  /**
+   * Retrieves all customers from the database.
+   *
+   * @returns List of all customers.
+   * @throws return message: return the list of customers or handle any errors during retrieval.
+   */
   async findAll() {
     return this.customerModel.find().populate('user');
   }
 
+  /**
+   * Retrieves a customer by its ID.
+   *
+   * @param id - The ID of the customer to retrieve.
+   * @returns The customer with the specified ID.
+   * @throws return message: return the customer object or an error if not found.
+   */
   async findOne(id: string) {
     return this.customerModel.findById(id).populate('user');
   }

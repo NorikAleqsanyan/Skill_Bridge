75d82b74bb2ed785475eddfa65720e8aa3e95382
NorikAleqsanyan <aleqsanyan.004@gmail.com>
2025-04-27 19:06:33 +0300

    I added comments JobService

--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -1,7 +1,5 @@
 import {
-  BadRequestException,
   Injectable,
-  NotFoundException,
 } from '@nestjs/common';
 import { CreateJobDto } from './dto/create-job.dto';
 import {
@@ -28,24 +26,43 @@ export class JobService {
     private mailService: MailService,
   ) {}
 
+  /**
+   * Creates a new job linked to a customer and skills.
+   *
+   * @param createJobDto - Job details.
+   * @param customerId - Customer ID.
+   * @returns Created job.
+   * @throws return message: or return message on validation errors.
+   */
+
   async create(createJobDto: CreateJobDto, customerId: string) {
     const { title, description, deadline, skills } = createJobDto;
 
     const customer = await this.customerModel.findById(customerId);
     if (!customer) {
-      throw new NotFoundException('Customer not found');
+      return {
+        message: 'User not found with that email',
+        error: true,
+      };
     }
-
     const foundSkills = await this.skillModel.find({
       _id: { $in: skills },
     });
 
     if (foundSkills.length !== skills.length) {
-      throw new NotFoundException('Some skills not found');
+      return {
+        message: 'Some skills not found',
+        error: true,
+      };
     }
+
     if (deadline && new Date(deadline) <= new Date()) {
-      throw new BadRequestException('The deadline must be in the future.');
+      return {
+        message: 'The deadline must be in the future.',
+        error: true,
+      };
     }
+
     const job = await this.jobModel.create({
       title,
       description,
@@ -62,8 +79,15 @@ export class JobService {
         $push: { jobs: job._id },
       });
     }
+
     return job;
   }
+
+  /**
+   * Retrieves all jobs with related skills and customer user data populated.
+   *
+   * @returns List of jobs.
+   */
 
   async findAll() {
     return await this.jobModel
@@ -72,6 +96,13 @@ export class JobService {
       .populate('customer.user');
   }
 
+  /**
+   * Retrieves a single job by ID with related skills and customer user data populated.
+   *
+   * @param jobId - Job ID.
+   * @returns The found job or null.
+   */
+
   async findOne(jobId: string) {
     return await this.jobModel
       .findById(jobId)
@@ -79,6 +110,12 @@ export class JobService {
       .populate('customer.user');
   }
 
+  /**
+   * Retrieves jobs by status if the status is valid.
+   *
+   * @param status - Job status.
+   * @returns List of jobs or an error message if status is invalid.
+   */
   async getJobsByStatus(status: StatusJob): Promise<any> {
     if (status >= 0 && status <= 2) {
       return this.jobModel.find({ status: status }).exec();
@@ -89,6 +126,12 @@ export class JobService {
     }
   }
 
+  /**
+   * Retrieves jobs associated with a customer or freelancer by user ID.
+   *
+   * @param userId - User ID.
+   * @returns List of jobs or an error message if user is not found.
+   */
   async getJobsByUser(userId: string): Promise<any> {
     const freelancer = await this.freelancerModel.findById(userId);
     const customer = await this.customerModel.findById(userId);
@@ -103,30 +146,63 @@ export class JobService {
     }
   }
 
+  /**
+   * Updates a job by ID after validating its existence and deadline.
+   *
+   * @param id - Job ID.
+   * @param updateJobDto - Updated job details.
+   * @returns The update result.
+   * @throws return message: or return message on validation errors.
+   */
+
   async update(id: string, updateJobDto: UpdateJobDto): Promise<any> {
     const job = await this.jobModel.findById(id).exec();
+
     if (!job) {
-      throw new NotFoundException(`Job not found`);
+      return {
+        message: 'Job not found',
+        error: true,
+      };
     }
+
     const { deadline } = updateJobDto;
+
     if (deadline && new Date(deadline) <= new Date()) {
-      throw new BadRequestException('The deadline must be in the future.');
+      return {
+        message: 'The deadline must be in the future.',
+        error: true,
+      };
     }
 
     return await this.jobModel.findByIdAndUpdate(id, updateJobDto);
   }
 
+  /**
+   * Adds multiple skills to a job after validating the job and skills.
+   *
+   * @param jobId - Job ID.
+   * @param addSkillDto - Skills to add.
+   * @returns Updated job with new skills.
+   * @throws return message: if the job or any skills are not found.
+   */
+
   async addSkillsInJob(jobId: string, addSkillDto: AddSkillDto): Promise<any> {
     const job = await this.jobModel.findById(jobId).exec();
     if (!job) {
-      throw new NotFoundException(`Job not found`);
+      return {
+        message: 'Job not found.',
+        error: true,
+      };
     }
     const { skills } = addSkillDto;
     if (skills) {
       const skill = await this.skillModel.find({ _id: { $in: skills } });
 
       if (skill.length !== skills.length) {
-        throw new NotFoundException('Some skills not found');
+        return {
+          message: 'Some skills not found',
+          error: true,
+        };
       }
     }
 
@@ -138,102 +214,170 @@ export class JobService {
     await this.jobModel.findByIdAndUpdate(job, {
       $push: { skills: skills },
     });
+
     return await this.jobModel.findById(jobId);
   }
 
+  /**
+   * Removes a skill from a job and updates both job and skill documents.
+   *
+   * @param jobId - Job ID.
+   * @param skillId - Skill ID.
+   * @returns Updated job.
+   * @throws return message: if the job or skill is not found.
+   */
+
   async deleteSkillsInJob(jobId: string, skillId: string) {
     const job = await this.jobModel.findById(jobId).exec();
+
     if (!job) {
-      throw new NotFoundException(`Job not found`);
+      return {
+        message: 'Job not found.',
+        error: true,
+      };
     }
+
     const skill = await this.skillModel.findById(skillId);
+
     if (!skill) {
-      throw new NotFoundException(`Skill not found`);
+      return {
+        message: 'Skill not found.',
+        error: true,
+      };
     }
+
     await this.skillModel.findByIdAndUpdate(skill, {
       $pull: { jobs: job._id },
     });
+
     await this.jobModel.findByIdAndUpdate(job, {
       $pull: { skills: skill._id },
     });
+
     return await this.jobModel.findById(jobId).exec();
   }
+
+  /**
+   * Assigns a freelancer to a job and updates the job and freelancer associations.
+   *
+   * @param jobId - Job ID.
+   * @param freelancerId - Freelancer ID.
+   * @returns Updated job with assigned freelancer.
+   * @throws return message: if the job or freelancer is not found.
+   */
 
   async assignFreelancerToJob(
     jobId: string,
     freelancerId: string,
-  ): Promise<Job> {
+  ): Promise<any> {
     const job = await this.jobModel.findById(jobId).exec();
+
     if (!job) {
-      throw new NotFoundException('Job not found');
+      return {
+        message: 'Job not found',
+        error: true,
+      };
     }
+
     const freelancer = await this.freelancerModel.findById(freelancerId).exec();
     if (!freelancer) {
-      throw new NotFoundException('Freelancer not found');
+      return {
+        message: 'Freelancer not found.',
+        error: true,
+      };
     }
 
     await this.jobModel.findByIdAndUpdate(job, { requestFreelancer: [] });
     await this.jobModel.findByIdAndUpdate(job, { freelancer: freelancer });
+
     await this.freelancerModel.findByIdAndUpdate(freelancer, {
       $push: { jobs: job },
     });
+
     job.requestFreelancer.forEach(async (elm) => {
       await this.freelancerModel.findByIdAndUpdate(elm, {
         $pull: { requestJob: job },
       });
     });
+
     await this.mailService.sendEmail(
       'aleqsanyan.004@gmail.com',
-      'assignFreelancerToJob',
-      'good',
+      'Freelancer Assigned to Job',
+      'Dear [Freelancer Name],\n\n' +
+        'We are pleased to inform you that you have been successfully assigned to the job titled "[Job Title]". Please review the job details and proceed accordingly.\n\n' +
+        'Best regards,\n' +
+        '[Your Company Name]',
     );
+
     return job;
   }
+
+  /**
+   * Updates the status of a job after validating the status value.
+   *
+   * @param jobId - Job ID.
+   * @param status - New job status.
+   * @returns Updated job.
+   * @throws return message: if the job is not found.
+   * @throws return message if the status is invalid.
+   */
 
   async updateStatus(jobId: string, status: StatusJob): Promise<any> {
     const job = await this.jobModel.findById(jobId);
     if (!job) {
-      throw new NotFoundException('Job not found');
+      return {
+        messasge: 'Job not found.',
+        error: true,
+      };
     }
+
     if (status >= 0 && status <= 2) {
       return await this.jobModel.findByIdAndUpdate(job, { status: status });
     } else {
-      throw new BadRequestException(
-        'Invalid status value. Status must be between 0 and 2.',
-      );
+      return {
+        messasge: 'Invalid status value. Status must be between 0 and 2.',
+        error: true,
+      };
     }
   }
 
   /**
+   * Adds feedback to a job if it is completed and belongs to the specified customer.
    *
-   *  
-   * 
-   * @param id:string
-   * @param feedbackDto:JobFeedbackDto
-   * @param customerId:string
-   * @returns Job|null
+   * @param id - Job ID.
+   * @param feedbackDto - Feedback data (rate and text).
+   * @param customerId - Customer ID who is providing the feedback.
+   * @returns Updated job with feedback or null.
+   * @throws return message: if the job or customer is not found, or if the job is not completed.
    */
+
   async addFeedbackToJob(
     id: string,
     feedbackDto: JobFeedbackDto,
     customerId: string,
-  ): Promise<Job | null> {
+  ): Promise<any> {
     const job = await this.jobModel.findById(id).populate('customer').exec();
 
     if (!job) {
-
-      throw new NotFoundException(`Job with ID ${id} not found`);
+      return {
+        messasge: `Job with ID ${id} not found`,
+        error: true,
+      };
     }
     const customer = await this.customerModel.findById(customerId);
     if (!customer) {
-
-      throw new NotFoundException('Customer not found');
+      return {
+        messasge: 'Customer not found',
+        error: true,
+      };
     }
     console.log(customerId, job.customer._id.toString());
 
     if (job.customer._id.toString() !== customerId) {
-
-      throw new NotFoundException(`This work does not belong to you`);
+      return {
+        messasge: `This work does not belong to you`,
+        error: true,
+      };
     }
     if (job.status === StatusJob.END) {
       await this.jobModel.findByIdAndUpdate(job, {
@@ -243,25 +387,42 @@ export class JobService {
         },
       });
     } else {
-
-      throw new NotFoundException('Job is not completed yet');
+      return {
+        messasge: 'Job is not completed yet․',
+        error: true,
+      };
     }
 
     return await this.jobModel.findById(id);
   }
 
+  /**
+   * Deletes a freelancer's request for a job and updates both job and freelancer documents.
+   *
+   * @param jobId - Job ID.
+   * @param freelancerId - Freelancer ID.
+   * @returns Updated job.
+   * @throws return message: if the job or freelancer is not found, or if the freelancer's request is not found.
+   */
+
   async deleteFreelancerRequest(
     jobId: string,
     freelancerId: string,
-  ): Promise<Job> {
+  ): Promise<any> {
     const job = await this.jobModel.findById(jobId);
     if (!job) {
-      throw new NotFoundException('Job not found');
+      return {
+        messasge: 'Job not found.',
+        error: true,
+      };
     }
 
     const freelancer = await this.freelancerModel.findById(freelancerId);
     if (!freelancer) {
-      throw new NotFoundException('Freelancer not found');
+      return {
+        messasge: 'Freelancer not found.',
+        error: true,
+      };
     }
 
     const requestIndex = job.requestFreelancer.findIndex(
@@ -269,7 +430,10 @@ export class JobService {
     );
 
     if (requestIndex === -1) {
-      throw new NotFoundException('Freelancer Request not found');
+      return {
+        messasge: 'Freelancer Request not found',
+        error: true,
+      };
     }
 
     await this.jobModel.findByIdAndUpdate(job, {
@@ -280,15 +444,27 @@ export class JobService {
     });
     await this.mailService.sendEmail(
       'aleqsanyan.004@gmail.com',
-      'Freelancer Request',
-      'rejected',
+      'Freelancer Request Rejected',
+      'Dear [Freelancer Name],\n\n' +
+        'We regret to inform you that your request for the job titled "[Job Title]" has been rejected by the customer.\n\n' +
+        'We encourage you to explore other job opportunities on the platform.\n\n' +
+        'Best regards,\n' +
+        '[Your Company Name]',
     );
+
     return job;
   }
 
-  async remove(id: string) {
-    console.log(id);
+  /**
+   * Removes a job and updates related customer, skills, and freelancer data.
+   * Only allows removal if the job is not started and has no freelancer assigned.
+   *
+   * @param id - Job ID.
+   * @returns True if the job is successfully removed, otherwise an error message.
+   * @throws Error if the job has already started or has a freelancer assigned.
+   */
 
+  async remove(id: string) {
     const job = await this.jobModel.findById(id);
     if (!job) {
       return {
@@ -315,22 +491,39 @@ export class JobService {
         $pull: { jobs: job._id },
       });
     });
+
     requestFreelancer.forEach(async (elm) => {
       await this.freelancerModel.findByIdAndUpdate(elm, {
         $pull: { jobs: job._id },
       });
     });
     await this.jobModel.findByIdAndDelete(job);
+
     return true;
   }
+
+  /**
+   * Toggles the block status of a job and updates related customer data.
+   * If the job is blocked, it moves it to the blocked jobs list and vice versa.
+   *
+   * @param jobId - Job ID.
+   * @returns A message indicating the job's block status.
+   * @throws return message: if the job is not found or already has a freelancer assigned.
+   */
 
   async isBlock(jobId: string) {
     const job = await this.jobModel.findById({ where: { jobId } });
     if (!job) {
-      throw new NotFoundException('Job not found');
+      return {
+        messasge: 'Job not found.',
+        error: true,
+      };
     }
     if (job.freelancer) {
-      throw new NotFoundException('This job has already Freelancer');
+      return {
+        messasge: 'This job has already Freelancer',
+        error: true,
+      };
     } else {
       if (job.isBlock) {
         await this.customerModel.findByIdAndUpdate(job.customer, {
@@ -354,9 +547,14 @@ export class JobService {
     }
     await this.mailService.sendEmail(
       'aleqsanyan.004@gmail.com',
-      'blocked',
-      'Job is blocked',
+      'Job Blocked',
+      'Dear [Customer Name],\n\n' +
+        'This is to inform you that the job titled "[Job Title]" has been blocked. Please review the job status, and you can unblock it anytime.\n\n' +
+        'If you have any questions, feel free to reach out to us.\n\n' +
+        'Best regards,\n' +
+        '[Your Company Name]',
     );
+
     return {
       message: 'Job is blocked',
     };
